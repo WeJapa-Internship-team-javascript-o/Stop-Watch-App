9dea356c718c1fb23c8fd664eadb9bc4f57a4e26
delat22 <babalolalateef18@gmai.com>
2020-08-05 16:22:27 +0100

    add lap space

--- scripts.js
+++ scripts.js
@@ -5,10 +5,15 @@ const resetButton = document.getElementById('reset');
 
 const lapButton = document.getElementById('lap');
 
+const lap = document.getElementById('laplist');
+
 startButton.addEventListener('click', ($event) => {
     startButton.textContent = 'Stop';
     resetButton.style.visibility = 'visible';
     lapButton.style.visibility = 'visible';
     
+})
 
+lapButton.addEventListener('click', ($event) => {
+    lap.style.visibility = 'visible';
 })
