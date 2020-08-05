7ec55c0e5b36ad02c9a51384576fd7aee7aa21ce
delat22 <babalolalateef18@gmai.com>
2020-08-05 09:08:17 +0100

    stop watch app

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-scripts.js
+
+const startButton = document.getElementById('start');
+
+const resetButton = document.getElementById('reset');
+
+const lapButton = document.getElementById('lap');
+
+startButton.addEventListener('click', ($event) => {
+    startButton.textContent = 'Stop';
+    resetButton.style.visibility = 'visible';
+    lapButton.style.visibility = 'visible';
+    
+
+})
