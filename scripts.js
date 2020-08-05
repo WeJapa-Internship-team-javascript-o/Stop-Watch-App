a52c585249808a3e155bde25e437689c9c2c633d
Worldsalt3 <riliwanhazzan@gmail.com>
2020-08-05 12:15:01 +0100

    adjusted the display width

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
+//get the start-button
 const startButton = document.getElementById('start');
-
+//get reset button
 const resetButton = document.getElementById('reset');
-
+//get lap button
 const lapButton = document.getElementById('lap');
 
+//event-listener for start-button
 startButton.addEventListener('click', ($event) => {
     startButton.textContent = 'Stop';
     resetButton.style.visibility = 'visible';
